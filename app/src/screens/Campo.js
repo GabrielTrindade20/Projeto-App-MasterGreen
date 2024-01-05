2060a4124e3ced27ab79c1af203b0211183fb276
Gabriel <gabriel.rochat18@gmai.com>
2024-01-05 18:44:36 -0300

    Finalização app

--- app/src/screens/Campo.js
+++ app/src/screens/Campo.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
+import { Dimensions } from 'react-native';
+
+const larguraDaTela = Dimensions.get('window').width;
+const alturaDaTela = Dimensions.get('window').height;
+
 
 export default props => {
-        return (
-            <View>
-                <Text>Tela Camo</Text>    
-            </View>
+    return (
+        <View style={styles.container}>
+            <Image source={require('../imagens/campo.jpg')} style={styles.imagemEstilo} />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imagemEstilo: {
+        width: larguraDaTela,
+        height: alturaDaTela, // Defina a altura desejada
+        resizeMode: 'contain', // Ou use 'cover' para cobrir o componente Image
+    },
+});
